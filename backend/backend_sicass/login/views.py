5946ebcdaaa52650bd21b5d134441e190905cc62
Julio César Hernández Girón <juliohernandez8520@gmail.com>
2021-06-21 10:06:10 -0600

    Conectado Select Tipo Servicio Social
    Creada:
    -Vista Tipo servicio social
    -Componente conectado al modelo
    -Instalado Axios para peticiones

--- backend/backend_sicass/login/views.py
+++ backend/backend_sicass/login/views.py
@@ -1,10 +1,15 @@
+from .models import TipoServicioSocial
 from django.shortcuts import render
 from rest_framework import viewsets
 from django.contrib.auth.models import User
-from .serializers import UsuarioSerializer
+from .serializers import UsuarioSerializer, TipoServicioSocialSerializer
 
 # Create your views here.
 
 class UsuarioVistas(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UsuarioSerializer
+
+class TipoServicioSocialVistas(viewsets.ModelViewSet):
+    queryset =  TipoServicioSocial.objects.all()
+    serializer_class = TipoServicioSocialSerializer
