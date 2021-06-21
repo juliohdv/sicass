b1840d5eb621b7bd28320dcbdee4e02dc7019914
Aragi group <aragidg@gmail.com>
2021-06-20 18:34:52 -0600

    Cambio de html con boostrap a react con boostrap para interfas de propuesta

--- frontend/frontend_sicass/src/componentes/Card.js
+++ frontend/frontend_sicass/src/componentes/Card.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
+import { Card } from "react-bootstrap";
 
-export default function Card(props) {
+export default function card(props) {
   return (
-    <div className="card">
+    /*<div className="card">
       <div className="card-header bg-info">
         <h3 className="card-tittle">{props.titulo}</h3>
       </div>
       <div className="card-body">
           { props.cuerpo }
       </div>
-    </div>
+    </div>*/
+
+    <Card border="info">
+      <Card.Header className="bg-info">{props.titulo}</Card.Header>
+      <Card.Body>
+        {props.cuerpo}
+      </Card.Body>
+    </Card>
   );
 }
--- frontend/frontend_sicass/src/componentes/EnvioPropuesta.js
+++ frontend/frontend_sicass/src/componentes/EnvioPropuesta.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Dashboard from "./Dashboard";
 import Card from "./Card";
+import { Row, Col, Form, Button } from "react-bootstrap";
 
 class Propuesta extends Component {
   render() {
@@ -12,10 +13,7 @@ function Cuerpo() {
   return (
     <div className="align-center">
       <div>
-        <Card 
-        titulo="Datos de entidad" 
-        cuerpo={<DatosEntidad />} 
-        />
+        <Card titulo="Datos de entidad" cuerpo={<DatosEntidad />} />
       </div>
       <div className="pt-4">
         <Card
@@ -23,15 +21,19 @@ function Cuerpo() {
           cuerpo={<DatosPropuesta />}
         />
       </div>
-      <div className="container pt-3">
-          <div className="row">
-              <div className="col text-right">
-                  <button type="Button" className="btn btn-success">Registrar</button>
-              </div>
-              <div className="col">
-              <button type="Button" className="btn btn-secondary">Cancelar</button>
-              </div>
+      <div className="container pt-4">
+        <div className="row">
+          <div className="col text-right pr-5">
+            <Button variant="success" type="button" onClick="">
+              Registrar
+            </Button>
           </div>
+          <div className="col pl-5">
+            <Button variant="secondary" type="button" onClick="">
+              Cancelar
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -39,7 +41,7 @@ function Cuerpo() {
 
 function DatosEntidad() {
   return (
-    <div className="container">
+    /*<div className="container">
       <div className="row">
         <div className="col">
           <label>Nombre de la entidad</label>
@@ -71,13 +73,47 @@ function DatosEntidad() {
           </select>
         </div>
       </div>
-    </div>
+    </div>*/
+
+    <Form className="pl-3 pr-3">
+      <Form.Row>
+        <Form.Group as={Col} className="pr-5">
+          <Form.Label>Nombre entidad</Form.Label>
+          <Form.Control type="text" placeholder="Ingrese nombre" />
+        </Form.Group>
+        <Form.Group as={Col}>
+          <Form.Label>Dirección</Form.Label>
+          <Form.Control type="text" placeholder="Ingrese dirección" />
+        </Form.Group>
+      </Form.Row>
+      <Form.Row>
+        <Form.Group as={Col} className="pr-5">
+          <Form.Label>Correo</Form.Label>
+          <Form.Control type="email" placeholder="Ingrese correo electronico" />
+        </Form.Group>
+        <Form.Group as={Col}>
+          <Form.Label>Teléfono</Form.Label>
+          <Form.Control type="text" placeholder="Ingrese teléfono" />
+        </Form.Group>
+      </Form.Row>
+      <Form.Row className="text-right">
+        <Form.Group as={Col} className="pr-5">
+          <Form.Label className="pt-2">Clasificación de la entidad</Form.Label>
+        </Form.Group>
+        <Form.Group as={Col}>
+          <Form.Control as="select">
+            <option>Privada</option>
+            <option>Pública</option>
+          </Form.Control>
+        </Form.Group>
+      </Form.Row>
+    </Form>
   );
 }
 
 function DatosPropuesta() {
   return (
-    <div className="container">
+    /*<div className="container">
       <div className="row">
         <div className="col mr-4">
           <div className="row">
@@ -115,7 +151,46 @@ function DatosPropuesta() {
           </div>
         </div>
       </div>
-    </div>
+    </div>*/
+    
+    <Form>
+      <Row>
+        <Col sm={6} className="pl-5">
+          <Form.Group as={Row}>
+            <Form.Label>Facultad</Form.Label>
+            <Form.Control as="select">
+              <option>Ingenieria y arquitectura</option>
+              <option>Biologia</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group as={Row}>
+            <Form.Label>Carrera</Form.Label>
+            <Form.Control as="select">
+              <option>Ingenieria informatica</option>
+              <option>Licenciatura en biologia</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group as={Row}>
+            <Form.Label>Tipo de servicio social</Form.Label>
+            <Form.Control as="select">
+              <option>Soporte técnico</option>
+              <option>Pasantias</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group as={Row}>
+            <Form.Label>Fecha limite para respuesta</Form.Label>
+            <Form.Control type="Date"></Form.Control>
+          </Form.Group>
+        </Col>
+        <Col sm={1}></Col>
+        <Col sm={5} className="pr-5">
+          <Form.Group as={Row}>
+            <Form.Label>Descripción de la propuesta</Form.Label>
+            <Form.Control as="textarea" rows={11}/>
+          </Form.Group>
+        </Col>
+      </Row>
+    </Form>
   );
 }
 
