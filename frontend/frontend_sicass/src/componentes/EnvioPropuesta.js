58f4afd063bca2dc04134f3fb5c37d94c8c02f19
Aragi group <aragidg@gmail.com>
2021-06-23 18:02:22 -0600

    Se arreglo los iconos en la tabla de rol, se cargan los datos de la bd en la tabla y creacion de acciones para crud

--- frontend/frontend_sicass/src/componentes/EnvioPropuesta.js
+++ frontend/frontend_sicass/src/componentes/EnvioPropuesta.js
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 import Dashboard from "./Dashboard";
 import Card from "./Card";
 import Entidad from "./Entidad";
-import SolicitudServicio from "./SolicitudServicio";
+import PropuestaServicio from "./PropuestaServicio";
 import { Row, Col, Form, Button } from "react-bootstrap";
 
 //Clase principal del componente
-class Propuesta extends Component {
+class EnvioPropuesta extends Component {
   render() {
     //Retorna todo la interfas respectiva para la solicitud de la propuesta
     return <Dashboard contenedor={<Cuerpo />} />;
@@ -20,59 +20,23 @@ function Cuerpo() {
     cuerpo referente a los campos de informacion de la entidad
     columnas referente a las columnas necesarias para maquetación de la interfaz
     descripcion hace referencia a los campos de informacion del servicio social a solicitar*/
-      <Form className="pl-5 pr-5">
-        <div className="pt-4">
+      <div className="pl-5 pr-5">
+        <div className="pt-4 ">
           <Card titulo="Datos de entidad" cuerpo={<Entidad />} />
         </div>
         <div className="pt-4">
           <Card
             titulo="Propuesta de servicio social"
             cuerpo={
-              <Row>
-                <SolicitudServicio columnas={6} />
-                <DatosPropuesta />
-              </Row>
+                <PropuestaServicio />
+              
             }
           />
         </div>
-        <Botones />
-      </Form>
- 
-  );
-}
-
-//Funcion que contiene el campo faltante para la propuesta "descripcion"
-function DatosPropuesta() {
-  return (
-    <>
-      <Col sm={1}></Col>
-      <Col sm={5} className="pr-5">
-        <Form.Group as={Row}>
-          <Form.Label>Descripción de la propuesta</Form.Label>
-          <Form.Control as="textarea" rows={11} maxLength="750" required />
-        </Form.Group>
-      </Col>
-    </>
-  );
-}
-
-function Botones(props){
-  return(
-    <div className="pt-4">
-      <div className="row">
-        <div className="col text-right pr-5">
-          <Button variant="success" type="submit" onClick="">
-            Registrar
-          </Button>
-        </div>
-        <div className="col pl-5">
-          <Button variant="secondary" type="button" onClick="">
-            Cancelar
-          </Button>
-        </div>
       </div>
-    </div>
   );
 }
 
-export default Propuesta;
+
+
+export default EnvioPropuesta;
--- frontend/frontend_sicass/src/componentes/EnvioRegistro.js
+++ frontend/frontend_sicass/src/componentes/EnvioRegistro.js
@@ -6,21 +6,12 @@ import Registro from "./Registro";
 //Clase principal del componente
 class EnvioRegistro extends Component {
   render() {
-    return(<Dashboard contenedor={<Cuerpo />} />); 
-  }
-}
-
-//Funcion que contiene los apartados del cuerpo de la interfaz
-function Cuerpo(props) {
-  return (
-    /* Se utilizan propiedades: 
-    cuerpo referente a los campos de informacion de la entidad
-    columnas referente a las columnas necesarias para maquetación de la interfaz
-    descripcion hace referencia a los campos de informacion del servicio social a solicitar*/
+    return(<Dashboard contenedor={
       <div className="pt-4 pl-5 pr-5">
         <Card titulo="Registro de Estudiante" cuerpo={<Registro />} />
       </div>
-  );
+    } />); 
+  }
 }
 
 export default EnvioRegistro;
