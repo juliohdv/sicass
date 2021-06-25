7f8cc1bf77aed6e0fbb1b70d92a9b9481e80cb36
Aragi group <aragidg@gmail.com>
2021-06-24 20:24:11 -0600

    Se eliminaron importaciones que no se utilizaban

--- frontend/frontend_sicass/src/componentes/Roles.js
+++ frontend/frontend_sicass/src/componentes/Roles.js
@@ -3,21 +3,14 @@ import Dashboard from "./Dashboard";
 import axios from "axios";
 import { Button, Form } from "react-bootstrap";
 import MaterialTable from "material-table";
-import AddBox from "@material-ui/icons/AddBox";
 import ArrowDownward from "@material-ui/icons/ArrowDownward";
-import Check from "@material-ui/icons/Check";
 import ChevronLeft from "@material-ui/icons/ChevronLeft";
 import ChevronRight from "@material-ui/icons/ChevronRight";
 import Clear from "@material-ui/icons/Clear";
-import DeleteOutline from "@material-ui/icons/DeleteOutline";
-import Edit from "@material-ui/icons/Edit";
 import FilterList from "@material-ui/icons/FilterList";
 import FirstPage from "@material-ui/icons/FirstPage";
 import LastPage from "@material-ui/icons/LastPage";
-import Remove from "@material-ui/icons/Remove";
-import SaveAlt from "@material-ui/icons/SaveAlt";
 import Search from "@material-ui/icons/Search";
-import ViewColumn from "@material-ui/icons/ViewColumn";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
 import Swal from "sweetalert2";
@@ -25,15 +18,6 @@ import { Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
 
 //Constante que contiene todo los iconos de la tabla de Datatable con material UI
 const tableIcons = {
-  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-  DetailPanel: forwardRef((props, ref) => (
-    <ChevronRight {...props} ref={ref} />
-  )),
-  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
   Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
   FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
   LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
@@ -44,8 +28,6 @@ const tableIcons = {
   ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
   Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
   SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
 };
 
 //Constante que contiene los datos estaticos de la tabla
@@ -190,6 +172,7 @@ class Roles extends Component {
         console.log(error);
       });
   }
+  
   render() {
     //Retorna todo la interfas respectiva para la gestion de roles y privilegios
     const { form } = this.state; //Constante que contiene el estado del formulario
